8d4f565422d60ab22f550dc042cc0597bf37889a
Arata Watanabe <watanabe.arata.73w@st.kyoto-u.ac.jp>
2023-05-05 00:27:34 +0900

    add: new function

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
@@ -1,4 +1,4 @@
-package jp.jaxa.iss.kibo.rpc.testapk;
+package jp.jaxa.iss.kibo.rpc.defaultapk;
 
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 // Kibo-RPC library
@@ -8,10 +8,10 @@ import gov.nasa.arc.astrobee.Result;
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 import jp.jaxa.iss.kibo.rpc.api.types.PointCloud;
-// astrobee library
+// astrobee library (for definition of Point and Quaternion etc.)
 
 import android.util.Log;
-// android library
+// android library (for log)
 
 import org.opencv.aruco.Aruco;
 import org.opencv.aruco.Dictionary;
@@ -21,11 +21,11 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.core.Rect;
 import static org.opencv.android.Utils.matToBitmap;
-// opencv library
+// opencv library (for detect ARmarkers)
 
 import java.util.ArrayList;
 import java.util.List;
-// java library
+// java library (for basic operate)
 
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
@@ -33,28 +33,40 @@ import java.util.List;
 
 public class YourService extends KiboRpcService {
 
+    // setting for log
     private final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void runPlan1() {
+        // remain start_log
         Log. i(TAG, "start mission");
 
         // the mission starts
         api.startMission();
 
-        // move to point 6
+        // definition of destination and quaternion
         Point point = new Point(11.355d, -8.9929d, 4.7818d);
         Quaternion quaternion = new Quaternion(0f, 0f, 0f, 1f);
 
+        // move to point 6
         api.moveTo(point, quaternion, false);
 
-        // get camera image
+        // get camera image (and save it)
+        Mat image = api.getMatNavCam();
+        api.saveMatImage(image, "target_6.png");
+
+        // definition of ARmarker's id and corners
         Mat ids = new Mat();
         List<Mat> corners = new ArrayList<>();
-        Mat image = api.getMatNavCam();
+
+        // definition of dictionary about ARmarkers
         Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
 
+        // detect ARmarker
         Aruco.detectMarkers(image, dictionary, corners, ids);
+
+        // spot laser
+        api.laserControl(true);
 
     }
 
