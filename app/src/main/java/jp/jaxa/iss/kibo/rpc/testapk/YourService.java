e7a869626d6e84e0804a3ea4bfd6948a91d6ccb0
Arata Watanabe <watanabe.arata.73w@st.kyoto-u.ac.jp>
2023-05-07 09:20:48 +0900

    Quartanionを修正

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
@@ -48,28 +48,28 @@ public class YourService extends KiboRpcService {
         // move to target_6
         Point point_6 = new Point(11.355d, -8.989d, 4.8305d);
         Quaternion quaternion_6 = new Quaternion(0f, 0f, 0f, 1f);
-        api.moveTo(point_6, quaternion_6, false);
+        api.moveTo(point_6, quaternion_6, true);
         Mat image = image_correction(api.getMatNavCam());
         api.saveMatImage(image, "target_6.png");
 
         // move to target_1
         Point point_1 = new Point(11.2746d, -9.92284d, 5.2988d);
         Quaternion quaternion_1 = new Quaternion(0f, 0f, -0.707f, 0.707f);
-        api.moveTo(point_1, quaternion_1, false);
+        api.moveTo(point_1, quaternion_1, true);
         image = image_correction(api.getMatNavCam());
         api.saveMatImage(image, "target_1.png");
 
         // move to taget_5
         Point point_5 = new Point(11.114d, -7.9756d, 5.3393d);
-        Quaternion quaternion_5 = new Quaternion(-0.707f, 0f, 0f, 0.707f);
-        api.moveTo(point_5, quaternion_5, false);
+        Quaternion quaternion_5 = new Quaternion(0.5f, 0.5f, -0.5f, 0.5f);
+        api.moveTo(point_5, quaternion_5, true);
         image = image_correction(api.getMatNavCam());
         api.saveMatImage(image, "target_5.png");
 
         // move to taget_4
         Point point_4 = new Point(10.51d, -6.7185d, 5.1804d);
-        Quaternion quaternion_4 = new Quaternion(1f, -1f, 0f, 0.707f);
-        api.moveTo(point_4, quaternion_4, false);
+        Quaternion quaternion_4 = new Quaternion(0f, 0f, -1f, 0f);
+        api.moveTo(point_4, quaternion_4, true);
         image = image_correction(api.getMatNavCam());
         api.saveMatImage(image, "target_4.png");
 
@@ -89,6 +89,9 @@ public class YourService extends KiboRpcService {
     }
 
     // 6-1-5-4でとりあえず行く
+
+    // 目標座標からのズレを修正するメソッド
+    public void moveTo() {}
     
     // 画像の歪み補正のメソッド
     public Mat image_correction(Mat image) {
