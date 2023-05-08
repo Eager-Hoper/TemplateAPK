6ba6b06f1c11b4478914e74b39eefa48f8d274a1
Arata Watanabe <watanabe.arata.73w@st.kyoto-u.ac.jp>
2023-05-08 18:28:55 +0900

    画像処理を改良

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
@@ -24,8 +24,10 @@ import static org.opencv.android.Utils.matToBitmap;
 import static org.opencv.imgproc.Imgproc.undistort;
 // opencv library (for detect ARmarkers)
 
+import java.security.IdentityScope;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Math;
 // java library (for basic operate)
 
 /**
@@ -49,39 +51,58 @@ public class YourService extends KiboRpcService {
         Point point_6 = new Point(11.355d, -8.989d, 4.8305d);
         Quaternion quaternion_6 = new Quaternion(0f, 0f, 0f, 1f);
         api.moveTo(point_6, quaternion_6, true);
-        Mat image = image_correction(api.getMatNavCam());
-        api.saveMatImage(image, "target_6.png");
+        
+        // 画像からターゲット中心からの相対座標を取得
+        double[] relative = getRelative(api.getMatNavCam());
+
+        // 現在地から相対座標分修正
+        Kinematics kinematics = api.getRobotKinematics();
+        Point real_point = kinematics.getPosition();
+        double dest_y = real_point.getY() + relative[0];
+        double dest_z = real_point.getZ() + relative[1];
+
+        // 新座標を指定
+        Point new_point = new Point(real_point.getX(), dest_y, dest_z);
+
+        // 再移動
+        api.moveTo(new_point, quaternion_6, true);
+        api.saveMatImage(image_correction(api.getMatNavCam()), "target_6.png");
+
+        // spot laser
+        api.laserControl(true);
+
+        // ミッション完了報告
+        api.reportMissionCompletion();
 
         // move to target_1
-        Point point_1 = new Point(11.2746d, -9.92284d, 5.2988d);
-        Quaternion quaternion_1 = new Quaternion(0f, 0f, -0.707f, 0.707f);
-        api.moveTo(point_1, quaternion_1, true);
-        image = image_correction(api.getMatNavCam());
-        api.saveMatImage(image, "target_1.png");
+        // Point point_1 = new Point(11.2746d, -9.92284d, 5.2988d);
+        // Quaternion quaternion_1 = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        // api.moveTo(point_1, quaternion_1, true);
+        // image = image_correction(api.getMatNavCam());
+        // api.saveMatImage(image, "target_1.png");
 
         // move to taget_5
-        Point point_5 = new Point(11.114d, -7.9756d, 5.3393d);
-        Quaternion quaternion_5 = new Quaternion(-0.5f, -0.5f, -0.5f, 0.5f);
-        api.moveTo(point_5, quaternion_5, true);
-        image = image_correction(api.getMatNavCam());
-        api.saveMatImage(image, "target_5.png");
+        // Point point_5 = new Point(11.114d, -7.9756d, 5.3393d);
+        // Quaternion quaternion_5 = new Quaternion(-0.5f, -0.5f, -0.5f, 0.5f);
+        // api.moveTo(point_5, quaternion_5, true);
+        // image = image_correction(api.getMatNavCam());
+        // api.saveMatImage(image, "target_5.png");
 
         // move to taget_4
-        Point point_4 = new Point(10.51d, -6.7185d, 5.1804d);
-        Quaternion quaternion_4 = new Quaternion(0f, 0f, -1f, 0f);
-        api.moveTo(point_4, quaternion_4, true);
-        image = image_correction(api.getMatNavCam());
-        api.saveMatImage(image, "target_4.png");
+        // Point point_4 = new Point(10.51d, -6.7185d, 5.1804d);
+        // Quaternion quaternion_4 = new Quaternion(0f, 0f, -1f, 0f);
+        // api.moveTo(point_4, quaternion_4, true);
+        // image = image_correction(api.getMatNavCam());
+        // api.saveMatImage(image, "target_4.png");
 
-        Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
-        Mat list_ids = new Mat();
-        List<Mat> corners = new ArrayList<>();
+        // Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+        // Mat list_ids = new Mat();
+        // List<Mat> corners = new ArrayList<>();
 
-        Aruco.detectMarkers(image, dictionary, corners, list_ids);
-        Aruco.drawDetectedMarkers(image, corners, list_ids);
+        // Aruco.detectMarkers(image, dictionary, corners, list_ids);
+        // Aruco.drawDetectedMarkers(image, corners, list_ids);
 
-        getCenter(list_ids, corners);
-
+        
         // spot laser
         api.laserControl(true);
 
@@ -109,53 +130,160 @@ public class YourService extends KiboRpcService {
         Mat correct_image = new Mat();
         undistort(image, correct_image, cameraMat, distortion);
 
+        Log.i(TAG, "get correct_image");
         return correct_image;
 
     }
 
-    // ARmarkarを認識して自己位置修正をするメソッド（開発中）
-    public void self_positioning(Mat image) {
+    // 画像を入力して、ターゲットの中心から現在地の誤差（double[2]={rx,ry}）を出力 ※画像内座標であることに注意
+    public double[] getRelative (Mat image) {
 
+        // 画像処理を行う
         Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
         Mat list_ids = new Mat();
         List<Mat> corners = new ArrayList<>();
+        Aruco.detectMarkers(image_correction(image), dictionary, corners, list_ids);
 
-        Aruco.detectMarkers(image, dictionary, corners, list_ids);
-        Aruco.drawDetectedMarkers(image, corners, list_ids);
+        // AR_infoを取得
+        double[][] AR_info = getAR_info(corners, list_ids);
+
+        // ターゲットの中心座標を取得
+        double[] target_center = getTargetCenter(AR_info, corners);
+
+        // 画像内での中有心からターゲットの中心までの差分（doubleで宣言しているが縮尺はpixel）を取得
+        double[] relative = new double[2];
+        relative[0] = 640 - target_center[0];
+        relative[1] = 480 - target_center[1];
+
+        // 縮尺をmeterに変換
+        double scale = getScale(corners);
+        relative[0] = relative[0] * scale;
+        relative[1] = relative[1] * scale;
+
+        Log.i(TAG, "get relative in picture");
+        return relative;
+
+    }
+
+    // AR_markerの四隅(corners)を入力して、AR_markerの中心座標（画像内座標）を出力
+    public double[] getAR_center (int order, List<Mat> corners) {
+
+        double[][] AR_corners =
+        {
+            {(int) corners.get(order).get(0,0)[0], (int) corners.get(order).get(0,0)[1]},
+            {(int) corners.get(order).get(0,1)[0], (int) corners.get(order).get(0,1)[1]},
+            {(int) corners.get(order).get(0,2)[0], (int) corners.get(order).get(0,2)[1]},
+            {(int) corners.get(order).get(0,3)[0], (int) corners.get(order).get(0,3)[1]},
+        };
 
         double sum_x = 0;
         double sum_y = 0;
         for (int i=0; i<4; i++) {
-            sum_x += corners.get(0).get(0,i)[0];
-            sum_y += corners.get(0).get(0,i)[1];
+            sum_x += AR_corners[i][0];
+            sum_y += AR_corners[i][1];
         }
-        double ARcenter_x = sum_x / 4;
-        double ARcenter_y = sum_y / 4;
+
+        double[] AR_center = new double[2];
+        AR_center[0] = sum_x / 4;
+        AR_center[1] = sum_y / 4;
+
+        Log.i(TAG, "get AR_center");
+        return AR_center;
+
+    }    
+
+    // 画像を入力して、AR_markerの情報(AR_info)を出力
+    // AR_infoについて　　1列目：AR_markerのID番号　2,3列目：AR_markerの中心座標（x,y）
+    public double[][] getAR_info (List<Mat> corners, Mat list_ids) {
+
+        // AR_info作成パート
+        int n = corners.size(); // 画像中にあるAR_markerの数
+        double[][] AR_info = new double[n][3]; // AR_infoを宣言
         
+        for (int i=0; i<n; i++) {
+
+            AR_info[i][0] = list_ids.get(0,i)[0]; // i行1列にID番号を代入
+
+            double[] AR_center = new double[2];
+            AR_center = getAR_center(n, corners); // AR_markerの中心座標を取得
+            AR_info[i][1] = AR_center[0]; // i行2列にi個目のAR_marker中心x座標を代入
+            AR_info[i][2] = AR_center[1]; // i行3列にi個目のAR_marker中心y座標を代入
+
+        }
+
+        Log.i(TAG, "get AR_info");
+        return AR_info;
+
     }
 
-    // 画像からARmarkerの情報（１行目：ID、２〜９行目：各コーナーの座標）（開発中）
-    public Mat getMarkerInfo(Mat list_ids, List<Mat> corners) {
+    // 各AR＿markerのIDと中心座標の情報(AR_info)を入力して、ターゲットの中心座標(target_center)を求める
+    public double[] getTargetCenter (double[][] AR_info, List<Mat> corners) {
 
-        Mat markerInfo = new Mat(4,9,CvType.CV_32FC1);
-        for (int i=0; i<4; i++) {
-            markerInfo.put(i,0,list_ids.get(i,0));
-            for (int j=0; j<8; j++) {
-                Mat Corner = corners.get(j);
-                markerInfo.put(i,1,Corner.get(i,j));
+        int n = corners.size();
+
+        for (int i=0; i<n; i++) {
+            int ID = AR_info[i][0];
+
+            // IDを4で割った余りが1ならx方向に-10cm分、y方向に+3.75cm分移動
+            if (ID%4 == 1) { 
+                AR_info[i][1] += -0.1/getScale(corners);
+                AR_info[i][2] += 0.0375/getScale(corners);
+
+            // IDを4で割った余りが2ならx方向に+10cm分、y方向に+3.75cm分移動
+            } else if (ID%4 == 2) {
+                AR_info[i][1] += 0.1/getScale(corners);
+                AR_info[i][2] += 0.0375/getScale(corners);
+
+            // IDを4で割った余りが3ならx方向に+10cm分、y方向に-3.75cm分移動
+            }else if (ID%4 == 3) {
+                AR_info[i][1] += 0.1/getScale(corners);
+                AR_info[i][2] += -0.0375/getScale(corners);
+
+            // IDを4で割った余りが0ならx方向に-10cm分、y方向に-3.75cm分移動
+            } else if (ID%4 == 0) {
+                AR_info[i][1] += -0.1/getScale(corners);
+                AR_info[i][2] += -0.0375/getScale(corners);
+
+            } else {
+                Log.i(TAG, "can't caluculate target_center");
             }
 
         }
-        
-        for (int i=0; i<4; i++) {
-            for (int j=0; j<4; j++) {
-                System.out.println(markerInfo.get(i, j));
-            }
-        }
 
-        return markerInfo;
+        double target_x, target_y;
+        
+        for (int i=0; i<n; i++) {
+            target_x += AR_info[i][1];
+            target_y += AR_info[i][2];
+        }
+        target_x = target_x / n;
+        target_y = target_y / n;
+
+        Log.i(TAG, "caluculate target_center");
+        return target_center(target_x, target_y); // 最終的に各AR_markerから推測したターゲットの中心座標の平均値を求めた
+
     }
 
+    // 画像の縮尺を求める（meter/pixel)
+    public double getScale (List<Mat> corners) {
+
+        double[][] AR_corners =
+        {
+            {(int) corners.get(0).get(0,0)[0], (int) corners.get(0).get(0,0)[1]}, // 左上
+            {(int) corners.get(0).get(0,1)[0], (int) corners.get(0).get(0,1)[1]}, // 右上
+            {(int) corners.get(0).get(0,2)[0], (int) corners.get(0).get(0,2)[1]}, // 右下
+            {(int) corners.get(0).get(0,3)[0], (int) corners.get(0).get(0,3)[1]}, // 左下
+        };
+
+        double side_length = ((AR_corners[1][0]-AR_corners[0][0])+(AR_corners[2][1]-AR_corners[1][1])+
+        (AR_corners[2][0]-AR_corners[3][0])+(AR_corners[3][1]-AR_corners[0][1])) / 4;
+
+        double scale = side_length / 0.05;
+
+        Log.i(TAG, "get Scale");
+        return scale;
+
+    }
 }
 
 // arataが編集したよ //
