c6fe671745e71381e17e5154504b8facd55be0dc
Kenshin Onishi <kenshin.onishi@gmail.com>
2023-06-03 16:49:17 +0900

    "We succeeded in finishing within time and reporting the correct message!"

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
@@ -20,7 +20,10 @@ import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.core.Rect;
+import org.opencv.objdetect.QRCodeDetector;
+
 import static org.opencv.android.Utils.matToBitmap;
+import static org.opencv.imgproc.Imgproc.boundingRect;
 import static org.opencv.imgproc.Imgproc.undistort;
 // opencv library (for detect ARmarkers)
 
@@ -42,7 +45,8 @@ public class YourService extends KiboRpcService {
     @Override
     protected void runPlan1() {
 
-        List<Long> time = new ArrayList<Long>();
+        //not considering the remaining time yet.
+        // List<Long> time = new ArrayList<Long>();
 
         // remain start_log
         Log. i(TAG, "arata: start mission");
@@ -58,7 +62,44 @@ public class YourService extends KiboRpcService {
         api.laserControl(true);
         api.takeTargetSnapshot(6);
         api.laserControl(false);
-        
+
+        //move to target_7(QRCode)
+        Point point_7 = new Point(11.369d, -8.5518d, 4.48d);
+        Quaternion quaternion_7 = new Quaternion(0f, 0.707f, 0f, 0.707f);
+        api.moveTo(point_7, quaternion_7, true);
+
+        //ReadQRCode
+        Mat QRimage = image_correction(api.getMatNavCam());
+        //Generate png image for dabug
+        api.saveMatImage(QRimage, "QR.png");
+
+        QRCodeDetector decoder = new QRCodeDetector();
+        String data = decoder.detectAndDecode(QRimage);
+
+        String reportMassage = null;
+        switch(data) {
+            case "JEM":
+                reportMassage = "STAY_AT_JEM";
+                break;
+            case "COLUMBUS":
+                reportMassage = "GO_TO_COLUMBUS";
+                break;
+            case "RACK1":
+                reportMassage = "CHECK_RUCK_1";
+                break;
+            case "ASTROBEE":
+                reportMassage = "I_AM_HERE";
+                break;
+            case "INTBALL":
+                reportMassage = "LOOKING_FORWARD_TO_SEE_YOU";
+                break;
+            case "BLANK":
+                reportMassage = "NO_PROBLEM";
+                break;
+            default:
+                break;
+        }
+
         // move to target_1
         Point point_1 = new Point(11.2053d, -9.92284d, 5.4736d);
         Quaternion quaternion_1 = new Quaternion(0f, 0f, -0.707f, 0.707f);
@@ -86,16 +127,19 @@ public class YourService extends KiboRpcService {
         api.takeTargetSnapshot(4);
         api.laserControl(false);
 
-        time = api.getTimeRemaining();
-        if(time.get(1) <30000){
-            Point Goal = new Point(11.143d, -6.7607d, 4.9654d);
-            Quaternion quaternion_Goal = new Quaternion(0f, 0f, -0.707f, 0.707f);
-            api.moveTo(Goal, quaternion_Goal, true);
-        }
+        //Declare we are approaching the goal(Required)
+        api.notifyGoingToGoal();
 
+        // move to the goal
+        Point Goal = new Point(11.143d, -6.7607d, 4.9654d);
+        Quaternion quaternion_Goal = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        api.moveTo(Goal, quaternion_Goal, true);
 
+        //report goal message and finish
+        api.reportMissionCompletion(reportMassage);
 
     }
+
 
     @Override
     protected void runPlan2() {
@@ -105,6 +149,22 @@ public class YourService extends KiboRpcService {
     @Override
     protected void runPlan3() {
         // write your plan 3 here
+    }
+
+    public Mat image_correction(Mat image) {
+
+        double[][] NavCamIntrinsics = api.getNavCamIntrinsics();
+        Mat cameraMat = new Mat(3, 3, CvType.CV_32FC1);
+        Mat distortion = new Mat(1, 5, CvType.CV_32FC1);
+        cameraMat.put(0, 0, NavCamIntrinsics[0]);
+        distortion.put(0, 0, NavCamIntrinsics[1]);
+
+        Mat correct_image = new Mat();
+        undistort(image, correct_image, cameraMat, distortion);
+
+        Log.i(TAG, "arata: get correct_image");
+        return correct_image;
+
     }
     
 }
