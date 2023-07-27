80f7640ac09d0cb6c93fabc3c46168c02668e861
arata <watanabe.arata.73w@st.kyoto-u.ac.jp>
2023-07-27 15:57:37 +0900

    improve error

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
@@ -639,12 +639,11 @@ public class YourService extends KiboRpcService {
         if (!(to == 7 || to == 8)) {
             try{
                 Thread.sleep(1000);
-            } catch(InturruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             api.laserControl(true);
             reMove_AR_moveTo(to, numberOfPhotos); // we can change reMove_AR_relativeMoveTo or reMove_AR_moveTo
-            laser_detect(numberOfPhotos);
             api.takeTargetSnapshot(to);
         }
 
@@ -873,7 +872,7 @@ public class YourService extends KiboRpcService {
 
         public void reMove_AR_relativeMoveTo ( int to){
 
-            double[] relative = getRelative(api.getMatNavCam());
+            double[] relative = getRelative(to, getMatNavCam());
             Point relative_dist = new Point(0, relative[0], relative[1]);
             Quaternion relative_orient = new Quaternion(0f, 0f, 0f, 0f);
 
@@ -888,7 +887,7 @@ public class YourService extends KiboRpcService {
             Quaternion quaternion3 = new Quaternion(0f, 0.707f, 0f, 0.707f);
             Quaternion quaternion4 = new Quaternion(0f, 0f, -1f, 0f);
 
-            double[] relative = getRelative(api.getMatNavCam());
+            double[] relative = getRelative(to,getMatNavCam());
             Kinematics kinematics = api.getRobotKinematics();
             Point current_point = kinematics.getPosition();
 
