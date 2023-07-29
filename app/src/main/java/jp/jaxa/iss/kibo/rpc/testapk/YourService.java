a50043f6395ca49ec3af51fe5c847ebdf2dc3ef4
arata <watanabe.arata.73w@st.kyoto-u.ac.jp>
2023-07-29 12:37:46 +0900

    improve error

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
@@ -645,8 +645,8 @@ public class YourService extends KiboRpcService {
             }
             api.laserControl(true);
             reMove_AR_moveTo(to, numberOfPhotos); // we can change reMove_AR_relativeMoveTo or reMove_AR_moveTo
+            //laser_detect(numberOfPhotos);
             api.takeTargetSnapshot(to);
-            api.laserControl(false);
         }
 
         //get timeRequired(LOG)
@@ -765,34 +765,15 @@ public class YourService extends KiboRpcService {
             }
         }
 
-        public double[] getRelative(int to) {
+        public double[] getRelative(int to, Mat image) {
 
-            /*　ARmarkerを認識してターゲット中心にレーザーが当たるような相対移動座標を計算するメソッド
-             * 
-             * @param
-             * to：目的地のターゲット番号
-             * 
-             * @return
-             * relative：現在地からターゲット中心までの移動座標
-             * 　relative[0]：画像内x方向を正とする相対座標
-             * 　relative[1]：画像内y方向を正とする相対座標
-            */
-
-            // ARmarkerを検出　(@see AR_detect)
+            // detect AR markers
             List<List<Mat>> AR_info = AR_detect(to);
             Mat ids = AR_info.get(0).get(0);
             List<Mat> corners = AR_info.get(1);
 
-            // ターゲット中心座標を計算　(@see getTargetCneter
             double[] target_center = getTargetCenter(ids, corners);
 
-            /*　astrobeeが修正する相対座標へ変換
-             * 
-             * 以下は各項の説明
-             * (target_center[] - ¥¥¥)：画像の中心からの相対座標を計算
-             * (target_center[] - ¥¥¥) / getScale()：縮尺をpixelからmeterに変更
-             * (target_center[] - ¥¥¥) / getScale() ± ¥¥¥：NavCam搭載位置とレーザー搭載位置の差分を修正
-             */
             double relative[] =
                     {   ((target_center[0] - 640) / getScale(corners)) - 0.0994,
                         ((target_center[1] - 480) / getScale(corners)) + 0.0285   };
@@ -804,155 +785,41 @@ public class YourService extends KiboRpcService {
 
         }
 
-        public List<List<Mat>> AR_detect(int to) {
-
-            /*　astrobeeの撮影した画像からARmarkerを検出する
-             * 
-             * @param
-             * to：目的地のターゲット番号
-             * 
-             * @return
-             * AR_info：ARmarkerのIDリスト（list_ids）と四隅座標のリスト（corners）を格納する
-             * List<List<Mat>> AR_info = {List<Mat> list_ids, List<Mat> corners}
-             */
-
-            // ARmarkerのdictionaryは5*5でIDが250番まであるものを使用
-            Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
-
-            // astrobeeカメラ画像を取得
-            Mat image = getMatNavCam();
-
-            // 取得画像からARmarkerを検出
-            Mat ids = new Mat();
-            List<Mat> corners = new ArrayList<>();
-            Aruco.detectMarkers(image_correction(image), dictionary, corners, ids);
-
-            /*　ARmarkerの検出に失敗した場合の処理
-             * 
-             * @throws
-             * corners == null || list_ids == null：以下の二つの原因でARmarker関連情報を取得できない場合
-             * 原因1, Aruco.detectMarkers()が画像を認識できずreturnがnull
-             * 原因2, astrobee取得画像内にARmarkerが存在しない
-             */
-
-            /*　原因1に対する対処
-             * Aruco.detectMarkers()を3回繰り返す
-             */
-            int loopCounter = 0;
-            int LOOP_MAX = 3;
-            while((corners == null || ids == null) && loopCounter < LOOP_MAX) {
-                Aruco.detectMarkers(image_correction(image), dictionary, corners, ids);
-                loopCounter++;
-            }
-
-            /*　原因2に対する対処
-             * ターゲット向かって後方に10cm移動して再度Aruco.detectMarkers()を3回まで繰り返す
-             * 
-             * TODO:後方に10cmではなくpivotPointへの移動でも良いのでは
-             */
-            if (corners == null || ids == null) {
-                Kinematics error_kinematics = api.getRobotKinematics();
-                Point error_point = error_kinematics.getPosition();
-                Quaternion quaternion1 = new Quaternion(0f, 0f, -0.707f, 0.707f);
-                Quaternion quaternion2 = new Quaternion(0.5f, 0.5f, -0.5f, 0.5f);
-                Quaternion quaternion3 = new Quaternion(0f, 0.707f, 0f, 0.707f);
-                Quaternion quaternion4 = new Quaternion(0f, 0f, -1f, 0f);
-
-                switch (to) {
-                    case 1:
-                        Point debug_point1 = new Point(error_point.getX(), error_point.getY()+0.10, error_point.getZ());
-                        api.moveTo(debug_point1, quaternion1, true);
-
-                    case 2:
-                        Point debug_point2 = new Point(error_point.getX(), error_point.getY(), error_point.getZ()+0.10);
-                        api.moveTo(debug_point2, quaternion2, true);
-
-                    case 3:
-                        Point debug_point3 = new Point(error_point.getX(), error_point.getY(), error_point.getZ()+0.10);
-                        api.moveTo(debug_point3, quaternion3, true);
-
-                    case 4:
-                        Point debug_point4 = new Point(error_point.getX()+0.10, error_point.getY(), error_point.getZ());
-                        api.moveTo(debug_point4, quaternion4, true);
-                }
-
-                while((corners == null || ids == null) && loopCounter < LOOP_MAX) {
-                    Aruco.detectMarkers(image_correction(image), dictionary, corners, ids);
-                    loopCounter++;
-                }
-
-            }
-
-            // return用に変数型を揃える
-            List<Mat> list_ids = new ArrayList<Mat>(Arrays.asList(ids));
-            List<List<Mat>> AR_info = new ArrayList<List<Mat>>(Arrays.asList(list_ids, corners));
-
-            return AR_info;
-
-        }
-
         public double[] getTargetCenter (Mat list_ids, List < Mat > corners){
 
-            /*　astrobeeの撮影した画像でのターゲットの中心座標を計算するメソッド
-             * 
-             * @param 
-             * list_ids：AR_detectで検出したARmarkerのID
-             * corners：AR_detectで検出したARmarkerの四隅の座標
-             * 
-             * @return
-             * target_center：画像でのターゲットの中心座標　(x,y)=(target_center[0],target_center[1])
-             */
-
-            // ARmarkerの数をnに代入
             int n = corners.size();
-
-            /*　画像が傾いていてもターゲット中心を計算できるように傾きを表す三角比を設定
-             * 
-             * @param
-             * a：斜辺
-             * b：短辺（x方向）
-             * c：長辺（y方向）
-             * sin：傾きの正弦
-             * cos：傾きの余弦
-             */
+            double[][] center_cand = new double[n][2];
+            double scale = getScale(corners);
 
             double a = Math.sqrt(Math.pow(corners.get(0).get(0, 3)[1] - corners.get(0).get(0, 0)[1], 2) + Math.pow(corners.get(0).get(0, 0)[0] - corners.get(0).get(0, 3)[0], 2));
             double b = corners.get(0).get(0, 0)[0] - corners.get(0).get(0, 3)[0];
             double c = corners.get(0).get(0, 3)[1] - corners.get(0).get(0, 0)[1];
-            double sin = b / a;
-            double cos = c / a;
 
-            /*　ARmarkerのIDを4で割った時の余りによってターゲット中心までの平行移動値を変更する
-             * 
-             * (±0.0125 * sin)：ターゲット中心のx方向への平行移動
-             * (±0.075  * cos)：ターゲット中心のy方向への平行移動
-             */
-
-            double[][] center_cand = new double[n][2];
-            double scale = getScale(corners);
+            double s = b / a;
+            double t = c / a;
 
             for (int i = 0; i < n; i++) {
                 double ID = list_ids.get(i, 0)[0];
 
                 // if ID≡1(mod4) UR, X=x-10[cm] and Y=y+3.75[cm]
                 if (ID % 4 == 1) {
-                    center_cand[i][0] = corners.get(i).get(0, 3)[0] + (-0.0125 * sin - 0.075 * cos) * scale;
-                    center_cand[i][1] = corners.get(i).get(0, 3)[1] + (0.0125 * cos - 0.075 * sin) * scale;
+                    center_cand[i][0] = corners.get(i).get(0, 3)[0] + (-0.0125 * s - 0.075 * t) * scale;
+                    center_cand[i][1] = corners.get(i).get(0, 3)[1] + (0.0125 * t - 0.075 * s) * scale;
 
                     // if ID≡2(mod4) UL, X=x+10[cm] and Y=y+3.75[cm]
                 } else if (ID % 4 == 2) {
-                    center_cand[i][0] = corners.get(i).get(0, 2)[0] + (-0.0125 * sin + 0.075 * cos) * scale;
-                    center_cand[i][1] = corners.get(i).get(0, 2)[1] + (0.0125 * cos + 0.075 * sin) * scale;
+                    center_cand[i][0] = corners.get(i).get(0, 2)[0] + (-0.0125 * s + 0.075 * t) * scale;
+                    center_cand[i][1] = corners.get(i).get(0, 2)[1] + (0.0125 * t + 0.075 * s) * scale;
 
                     // if ID≡3(mod4) BL, X=x+10[cm] and Y=y-3.75[cm]
                 } else if (ID % 4 == 3) {
-                    center_cand[i][0] = corners.get(i).get(0, 1)[0] + (0.0125 * sin + 0.075 * cos) * scale;
-                    center_cand[i][1] = corners.get(i).get(0, 1)[1] + (-0.0125 * cos + 0.075 * sin) * scale;
+                    center_cand[i][0] = corners.get(i).get(0, 1)[0] + (0.0125 * s + 0.075 * t) * scale;
+                    center_cand[i][1] = corners.get(i).get(0, 1)[1] + (-0.0125 * t + 0.075 * s) * scale;
 
                     // if ID≡0(mod4) BR, X=x-10[cm] and Y=y-3.75[cm]
                 } else if (ID % 4 == 0) {
-                    center_cand[i][0] = corners.get(i).get(0, 0)[0] + (0.0125 * sin - 0.075 * cos) * scale;
-                    center_cand[i][1] = corners.get(i).get(0, 0)[1] + (-0.0125 * cos - 0.075 * sin) * scale;
+                    center_cand[i][0] = corners.get(i).get(0, 0)[0] + (0.0125 * s - 0.075 * t) * scale;
+                    center_cand[i][1] = corners.get(i).get(0, 0)[1] + (-0.0125 * t - 0.075 * s) * scale;
 
                 } else {
                     // TODO: Concern what to do if camera can't find AR marker
@@ -961,9 +828,10 @@ public class YourService extends KiboRpcService {
 
             }
 
-            /*　各ARmarkerの四隅座標でのターゲット中心座標を求めたら、その平均値をtarget_centerに代入する */
+            // get average of center_candidate
             double target_x = 0;
             double target_y = 0;
+
             for (int i = 0; i < n; i++) {
                 target_x += center_cand[i][0];
                 target_y += center_cand[i][1];
@@ -975,18 +843,9 @@ public class YourService extends KiboRpcService {
 
         }
 
-        public double getScale (List<Mat> corners) {
+        // get scale in image (pixel/meter)
+        public double getScale (List < Mat > corners) {
 
-            /*　astrobeeの撮影した画像内座標とISS内座標の縮尺を計算するメソッド
-             * 
-             * @param
-             * corners：AR_detectで検出したARmarkerの四隅の座標
-             * 
-             * @return
-             * scale：ISS内座標から画像内座標への縮尺 単位は[pixel/meter]
-             */
-
-            // cornersを扱いやすい変数型に変更
             double[][] AR_corners =
                     {
                             {(int) corners.get(0).get(0, 0)[0], (int) corners.get(0).get(0, 0)[1]}, // UL
@@ -995,8 +854,6 @@ public class YourService extends KiboRpcService {
                             {(int) corners.get(0).get(0, 3)[0], (int) corners.get(0).get(0, 3)[1]}, // BL
                     };
 
-            // ARmarkerの４辺の長さを足し上げる　
-            // @param  side_length：ARmarkerの周長
             double side_length = 0;
             for (int i = 0; i < 4; i++) {
                 if (i < 3) {
@@ -1006,7 +863,6 @@ public class YourService extends KiboRpcService {
                 }
             }
 
-            // 周長が20cm（5cm * 4辺）であることから縮尺を計算
             double scale = side_length / (4 * 0.05);
 
             Log.i(TAG, "-------------- DEBUG: scale[pixel/meter]=" + scale);
@@ -1018,19 +874,7 @@ public class YourService extends KiboRpcService {
 
         public void reMove_AR_relativeMoveTo (int to){
 
-            /*　api.relativeMoveTo()を用いてターゲット前での自己位置修正を行うメソッド
-             * 
-             * @param
-             * to：目的地のターゲット番号
-             * 
-             * @return
-             * void：自己位置修正を行う
-             * 
-             * @deprecated
-             * api.relativeMoveTo()は位置情報が不正確な場合、処理に最大30秒かかることから採用していない
-             */
-
-            double[] relative = getRelative(to);
+            double[] relative = getRelative(to, api.getMatNavCam());
             Point relative_dist = new Point(0, relative[0], relative[1]);
             Quaternion relative_orient = new Quaternion(0f, 0f, 0f, 0f);
 
@@ -1040,31 +884,17 @@ public class YourService extends KiboRpcService {
 
         public void reMove_AR_moveTo (int to, int numberOfPhotos){
 
-            /*　api.moveTo()を用いてターゲット前での自己位置修正を行うメソッド
-             * 
-             * @param
-             * to：目的地のターゲット番号
-             * 
-             * @return
-             * void：自己位置修正を行う
-             */
-
             Quaternion quaternion1 = new Quaternion(0f, 0f, -0.707f, 0.707f);
             Quaternion quaternion2 = new Quaternion(0.5f, 0.5f, -0.5f, 0.5f);
             Quaternion quaternion3 = new Quaternion(0f, 0.707f, 0f, 0.707f);
             Quaternion quaternion4 = new Quaternion(0f, 0f, -1f, 0f);
 
-            // ターゲットまでの相対位置を取得
-            double[] relative = getRelative(to);
-
+            double[] relative = getRelative(to, api.getMatNavCam());
             Kinematics kinematics = api.getRobotKinematics();
             Point current_point = kinematics.getPosition();
 
             Log.i(TAG, "-------------- DEBUG: current_point=" + current_point);
 
-            /*　目的地のターゲット番号によってx,y,z座標の修正を場合分けしている
-             *　また、位置修正時に5cmターゲット方向に前進する（最小移動距離確保のため）
-             */
             switch (to) {
                 case 1:
                     api.saveMatImage(image_correction(getMatNavCam()), numberOfPhotos + ":target1Image__before.png");
@@ -1106,7 +936,6 @@ public class YourService extends KiboRpcService {
                     break;
             }
 
-            // for DEBUG　本番実装時には除いて良い
             Kinematics kinematics_after = api.getRobotKinematics();
             Point after_point = kinematics_after.getPosition();
 
@@ -1114,6 +943,63 @@ public class YourService extends KiboRpcService {
             Log.i(TAG, "-------------- DEBUG: current_pointとの差分がrelative[](in real)と同じであれば移動は成功");
 
         }
+
+        public List<List<Mat>> AR_detect(int to) {
+
+            Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+            Mat ids = new Mat();
+            List<Mat> corners = new ArrayList<>();
+            Mat image = getMatNavCam();
+
+            Aruco.detectMarkers(image_correction(image), dictionary, corners, ids);
+
+            int loopCounter = 0;
+            int LOOP_MAX = 3;
+            while((corners == null || ids == null) && loopCounter < LOOP_MAX) {
+                Aruco.detectMarkers(image_correction(image), dictionary, corners, ids);
+                loopCounter++;
+            }
+
+            if (corners == null || ids == null) {
+                Kinematics error_kinematics = api.getRobotKinematics();
+                Point error_point = error_kinematics.getPosition();
+                Quaternion quaternion1 = new Quaternion(0f, 0f, -0.707f, 0.707f);
+                Quaternion quaternion2 = new Quaternion(0.5f, 0.5f, -0.5f, 0.5f);
+                Quaternion quaternion3 = new Quaternion(0f, 0.707f, 0f, 0.707f);
+                Quaternion quaternion4 = new Quaternion(0f, 0f, -1f, 0f);
+
+                switch (to) {
+                    case 1:
+                        Point debug_point1 = new Point(error_point.getX(), error_point.getY()+0.10, error_point.getZ());
+                        api.moveTo(debug_point1, quaternion1, true);
+
+                    case 2:
+                        Point debug_point2 = new Point(error_point.getX(), error_point.getY(), error_point.getZ()+0.10);
+                        api.moveTo(debug_point2, quaternion2, true);
+
+                    case 3:
+                        Point debug_point3 = new Point(error_point.getX(), error_point.getY(), error_point.getZ()+0.10);
+                        api.moveTo(debug_point3, quaternion3, true);
+
+                    case 4:
+                        Point debug_point4 = new Point(error_point.getX()+0.10, error_point.getY(), error_point.getZ());
+                        api.moveTo(debug_point4, quaternion4, true);
+                }
+
+                while((corners == null || ids == null) && loopCounter < LOOP_MAX) {
+                    Aruco.detectMarkers(image_correction(image), dictionary, corners, ids);
+                    loopCounter++;
+                }
+
+            }
+
+            List<Mat> list_ids = new ArrayList<Mat>(Arrays.asList(ids));
+            List<List<Mat>> AR_info = new ArrayList<List<Mat>>(Arrays.asList(list_ids, corners));
+
+            return AR_info;
+
+        }
+
 
         // NULL check
         public Mat getMatNavCam () {
@@ -1130,4 +1016,3 @@ public class YourService extends KiboRpcService {
         }
 
     }
-
