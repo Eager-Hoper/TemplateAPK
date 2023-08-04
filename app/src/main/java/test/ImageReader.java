f5c8979972ecdfade9bb30a36b141df14bc1e303
your name <you@gmail.com>
2023-08-04 13:20:10 +0900

    add; opencvtest doesnt work well

--- app/src/main/java/test/ImageReader.java
+++ app/src/main/java/test/ImageReader.java
@@ -1,17 +1,21 @@
 package test;
 import org.opencv.core.Mat;
-import org.opencv.core.MatVector;
-import org.opencv.core.Scalar;
-import org.opencv.core.highgui;
-import org.opencv.core.opencv_imgcodecs.*;
+import org.opencv.core.Core;
+import org.opencv.imgcodecs.Imgcodecs;
+//import org.opencv.core.opencv_imgcodecs.*;
 
 public class ImageReader {
 
     public static void main(String[] args) {
         String imageFilePath = "sample.png";
 
+        //Loading the OpenCV core library
+        System.out.println(Core.NATIVE_LIBRARY_NAME);
+        System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+
         // 画像を読み込む
-        Mat image = imread(imageFilePath);
+        Imgcodecs imageCodecs = new Imgcodecs();
+        Mat image = imageCodecs.imread(imageFilePath);
 
         // 画像の幅と高さを取得
         int width = image.cols();
@@ -20,7 +24,7 @@ public class ImageReader {
 
         // 画像の処理や表示などの操作をここで行う
         // 例: ウィンドウに画像を表示する
-        opencv_highgui.imshow("Image", image);
-        opencv_highgui.waitKey();
+        // opencv_highgui.imshow("Image", image);
+        // opencv_highgui.waitKey();
     }
 }
